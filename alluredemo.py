cb014808297ffcf1d676302c5f77070d001399d0
unknown <songxudan@Koolearn-inc.com>
2019-04-09 18:21:58 +0800

    修改说明

---
+++ alluredemo.py
@@ -0,0 +1,25 @@
+#! /usr/bin/python
+# coding:utf-8
+"""
+@author:Bingo.he
+@file: test_allure.py
+@time: 2018/07/12
+"""
+import unittest
+
+class DivTest(unittest.TestCase):
+
+    def test_div_001(self):
+        self.assertEqual(1, 1)
+
+    def test_div_002(self):
+        self.assertEqual(2, 2)
+
+    def test_div_003(self):
+        self.assertEqual(3, 3)
+
+    def test_div_004(self):
+        self.assertEqual(4, 4)
+
+    def test_div_005(self):
+        self.assertEqual(3, 3)
